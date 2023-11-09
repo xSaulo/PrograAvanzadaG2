5a2a18828dbd124380abe0867b3d34f71e049fb1
Saul Delgado Calvo <118625634+xSaulo@users.noreply.github.com>
2023-11-08 23:17:55 -0600

    Creacion de Proyecto

---
+++ ProyectoG2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProyectoG2.MvcApplication" Language="C#" %>
---
+++ ProyectoG2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProyectoG2.Startup))]
+namespace ProyectoG2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
